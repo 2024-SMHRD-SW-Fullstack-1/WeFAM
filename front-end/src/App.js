0e711dbfdb864a044625dbaa14853d9f63e9bc3d
김재효 <67933778+JaehyoDev@users.noreply.github.com>
2024-08-22 12:02:23 +0900

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,22 @@
-import Header from "./components/Header";
-import LeftSidebar from "./components/LeftSidebar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Main from "./components/Main";
-import RightSidebar from "./components/RightSidebar";
+import Memo from "./components/memo/Memo";
+import Recipe from "./components/Recipe";
 import "./css/App.module.css";
 import "./css/Test.css";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <LeftSidebar></LeftSidebar>
-      <Main></Main>
-      <RightSidebar></RightSidebar>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* 중첩된 라우트들 */}
+          <Route index element={<Main />} />
+          <Route path="memo" element={<Memo />} />
+          <Route path="recipe" element={<Recipe />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
