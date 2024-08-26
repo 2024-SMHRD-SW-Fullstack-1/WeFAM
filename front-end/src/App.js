af8ec471eec5c92f22de5d15a1f21d9eb3f61172
김재효 <67933778+JaehyoDev@users.noreply.github.com>
2024-08-26 14:48:42 +0900

    jaehyo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Main from "./components/Main";
+import Feed from "./components/feed/Feed";
 import Calendar from "./components/calendar/Calendar";
 import Memo from "./components/memo/Memo";
 import Todo from "./components/todo/Todo";
 import Recipe from "./components/recipe/Recipe";
 import Galary from "./components/galary/Galary";
+import Login from "./components/login/Login";
 import "./css/App.css";
 import "./css/Test.css";
-import LeftSidebar from "./components/LeftSidebar";
-import RightSidebar from "./components/RightSidebar";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<Main />} />
+          <Route index element={<Feed />} />
           <Route path="calendar" element={<Calendar />} />
           <Route path="memo" element={<Memo />} />
           <Route path="todo" element={<Todo />} />
           <Route path="recipe" element={<Recipe />} />
           <Route path="galary" element={<Galary />} />
         </Route>
-        <Route path="login" element={<LogIn />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
