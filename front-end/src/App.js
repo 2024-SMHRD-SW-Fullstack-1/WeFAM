f728fa4ded1c9b149d37f20b199655d0faa137af
cheollll <tmd020417@gmail.com>
2024-09-03 18:02:40 +0900

    0903 1800

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,19 +8,21 @@ import AlbumFolder from "./components/album/AlbumFolder";
 import Login from "./components/login/Login";
 import RightSidebar from "./components/right-sidebar/RightSidebar";
 import Settings from "./components/user-setting/Settings";
+import HouseWork2 from "./components/housework/HouseWork2";
 import "./css/App.css";
 import "./css/Test.css";
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="main" element={<Home />}>
+        <Route path='/' element={<Login />} />
+        <Route path='main' element={<Home />}>
           <Route index element={<Feed />} />
-          <Route path="calendar" element={<Calendar />} />
-          <Route path="housework" element={<HouseWork />} />
-          <Route path="album" element={<AlbumFolder />} />
-          <Route path="settings" element={<Settings />} />
+          <Route path='calendar' element={<Calendar />} />
+          <Route path='housework' element={<HouseWork />} />
+          <Route path='album' element={<AlbumFolder />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='housework2' element={<HouseWork2 />} />
         </Route>
       </Routes>
     </div>
